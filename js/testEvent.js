43e6885e2784c170b184f2fa0e6cd2e841a426b4
Mystic Weng <710120644@qq.com>
2017-03-06 23:13:44 +0800

    update js

--- js/testEvent.js
+++ js/testEvent.js
@@ -1,4 +1,5 @@
 window.onload = prepareLinks
+window.onload = prepareImages
 
 // after window loaded dom
 function prepareLinks(){
@@ -16,3 +17,39 @@ function prepareLinks(){
 function popUp(href){
     window.open(href, 'popup', 'width=200, height=100')
 }
+// image gallery
+function prepareImages(){
+    // method check
+    if(!document.getElementsByTagName || !document.getElementById){
+        return false
+    }
+    // element check
+    if(!document.getElementById('imagegallery')){
+        return false
+    }
+
+    var gallery = document.getElementById('imagegallery')
+    var links = gallery.getElementsByTagName('a')
+    // links.forEach((link) => {
+    //     link.onclick = function() {
+    //         showPic(this)
+    //         return false
+    //     }
+    // })
+    for(let i = 0; i<links.length; i++){
+        links[i].onclick = function() {
+            showPic(this)
+            return false
+        }
+    }
+}
+
+function showPic(whichPic){
+    var source = whichPic.getAttribute('href')
+    var placeholder = document.getElementById('placeholder')
+    placeholder.setAttribute('src',source)
+    var text = whichPic.getAttribute('title')
+    var description = document.getElementById('description')
+    description.firstChild.nodeValue = text
+}
+
